fa0c09066b36f27a1e5b017989fa5d8f40a85c2b
level7up <abdoleemos200@gmail.com>
2020-05-17 12:28:22 +0200

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,24 +16,40 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNubmer = 1;
+  void press() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNubmer = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: FlatButton(
-                child: Image.asset('images/dice1.png'),
-              ),
+            child: FlatButton(
+              onPressed: () {
+                press();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice4.png'),
+            child: FlatButton(
+              onPressed: () {
+                press();
+              },
+              child: Image.asset('images/dice$rightDiceNubmer.png'),
             ),
           ),
         ],
@@ -40,3 +57,5 @@ class DicePage extends StatelessWidget {
     );
   }
 }
+
+//class DicePage extends StatelessWidget {}
